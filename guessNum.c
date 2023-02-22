40512bd7a23fab6e248bccb4604e021309a806aa
josh24311 <josh24311@gmail.com>
2023-02-22 17:16:55 +0800

    new guessNum.c practice in L61

---
+++ guessNum.c
@@ -0,0 +1,14 @@
+/*C language practice in L61 */
+#include<stdio.h>
+
+int main(){
+	int input,ans = 5;//set answer as 5
+	printf("Please enter a integer number:\n");
+	scanf("%d",&input);
+	if(input > ans)	printf("Too large!\n");
+	else{
+		if(guess < ans)	printf("Too small\n");
+		else	printf("Correct answer!\n");
+	} 
+	return 0;
+}
