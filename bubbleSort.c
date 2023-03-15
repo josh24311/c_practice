7b28ba90a9c606c26a26505221e8e876c96f3d88
josh24311 <josh24311@gmail.com>
2023-03-15 09:40:24 +0800

    0315 add 2 files

---
+++ bubbleSort.c
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//L123 sort 5 int numbers
+#include <stdio.h>
+int all=5;
+int main()
+{
+    int i, arr[5]={0},j,tmp,fl=0;
+    printf("Enter %d number:\n",all);
+    for(i=0;i<all;i++){
+        scanf("%d",&arr[i]);
+        
+    }
+    for(i=0;i<all-1;i++){
+        for(j=0;j<all-i-1;j++){
+            if(arr[j] > arr[j+1]){
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+        
+    }
+    
+    printf("After bubble sort\n");
+    for(i=0;i<all;i++){
+        printf("%d ",arr[i]);
+    }
+    
+   
+    
+    
+    return 0;
+}
