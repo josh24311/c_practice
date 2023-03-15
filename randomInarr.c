cafa1ef2216fc84bb3dac52aac8d5e72ff1a0120
josh24311 <josh24311@gmail.com>
2023-03-15 09:43:42 +0800

    randomarr ver2

--- randomInarr.c
+++ randomInarr.c
@@ -13,12 +13,12 @@ Write your code in this editor and press "Run" button to compile and execute it.
 int main()
 {
     srand(time(0));
-    printf("Hello World\n");
+    //printf("Hello World\n");
     int a[10] = {1,2,3,4,5,6,7,8,9,10};
     int now,tmp,i;
     for(i=0;i<10;++i){
         
-            now = rand() % 10 ;
+            now = rand() % (10-i) + i ;
             tmp = a[i];
             a[i] = a[now];
             a[now] = tmp;
@@ -27,5 +27,6 @@ int main()
     for(i=0;i<10;++i){
         printf("%d ",a[i]);
     }
+    printf("\n");
     return 0;
 }
