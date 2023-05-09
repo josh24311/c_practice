5b9f1a8e74e4167d35a001f0289e7900295213b2
陳力瑋 <josh24311@gmail.com>
2023-05-09 22:52:31 +0800

    Update LC1_singleNumber.c
    revise @ 230509

--- LC1_singleNumber.c
+++ LC1_singleNumber.c
@@ -1,23 +1,34 @@
 /******************************************************************************
+Leetcode April 1 : Single number
+Given a non-empty array of integers nums, every element appears twice except for one. 
+Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
 *******************************************************************************/
 
 #include <stdio.h>
-
+int singleNumber(int*, int);
 int main()
 {
-    printf("Hello World");
+    int na,num;
+	int a[3] = {2,2,1};     // num = 3
+	int b[5] = {4,1,2,1,2}; // num = 5
+	int c[1] = {1};         // num = 1
+	
+    printf("Single number is : %d",singleNumber(b,5));
 
     return 0;
 }
 int singleNumber(int* nums, int numSize){
-    int n = nms[0];
+    int n = nums[0];
     for(int i = 1; i < numSize; i++){
-        n ^= num[i];
+        n ^= nums[i];
     }
     return n;
 }
