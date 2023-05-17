3b5a8c80e03d3ef8a66e87c5f9ab872abe0bb1e6
josh24311 <josh24311@gmail.com>
2023-05-17 09:44:50 +0800

    LC5_2_1

--- LC5_maxProfit_2.c
+++ LC5_maxProfit_2.c
@@ -1,8 +1,12 @@
 /******************************************************************************
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+
+
+
 
 *******************************************************************************/
 
